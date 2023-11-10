cfd170899bea518f044359f01c4ce9d7f85ca11a
Patrick Schleizer <adrelanos@whonix.org>
2023-11-10 12:35:12 -0500

    CodeQL

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 set -x
 set -e
 
-apt update --error-on=any
-apt install --yes libevdev2 libevdev-dev libsodium23 libsodium-dev pkg-config
+sudo --non-interactive apt-get update --error-on=any
+sudo --non-interactive apt-get install --yes libevdev2 libevdev-dev libsodium23 libsodium-dev pkg-config
 
 make
